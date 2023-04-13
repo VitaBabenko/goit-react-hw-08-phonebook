57a78bc0701f191a194f6ce726e07dd3c958425d
VitaBabenko <babenkovita1988@gmail.com>
2023-04-13 23:57:56 +0300

    +

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,24 +3,24 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/auth-slice';
 import { contactsReducer } from './contacts/contactsSlice';
-// import { filterReducer } from './contacts/filterSlice';
+import { filterReducer } from './contacts/filterSlice';
 
 const authPersistConfig = {
   key: 'auth',
   storage,
 };
 
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-//   filter: filterReducer,
-// });
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
 
 const persistedReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     auth: persistedReducer,
-    contacts: contactsReducer,
+    contacts: rootReducer,
   },
 });
 
